76a12c0fa8bcd41a2491646ad677d3fad27e5fe5
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-16 22:55:28 -0700

    playRound function Checks for player roll and computer roll, determines a winner, and returns a win/loss string. Checks for invalid player input.

--- javascript.js
+++ javascript.js
@@ -12,3 +12,27 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    PlayerSelection = (playerSelection.substring(0, 1).toUpperCase() + playerSelection.substr(1).toLowerCase()).replace( /\s/g, '')
+
+    // Winning conditions for the player
+    if (PlayerSelection === "Rock" && computerSelection === "Scissors" 
+        || PlayerSelection === "Paper" && computerSelection === "Rock"
+        || PlayerSelection === "Scissors" && computerSelection === "Paper") {
+            return `You won! You chose ${PlayerSelection}, which beats ${computerSelection}`
+        } 
+
+    // Winning conditions for the computer
+        else if (PlayerSelection === "Rock" && computerSelection === "Paper" 
+        || PlayerSelection === "Paper" && computerSelection === "Scissors"
+        || PlayerSelection === "Scissors" && computerSelection === "Rock") {
+            return `You lose! The computer picked ${computerSelection}, which beats ${PlayerSelection}.`
+        }
+        else {
+            return `Are you sure that's a valid move? ${PlayerSelection} isn't rock, paper, or scissors!`
+        }
+
+}
+
+
+console.log(playRound("Rock", "Paper"))
